da778d0fc419775014349eae5466ac09ab8bcf9b
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-06-07 11:19:36 +0530

    TailwindCSS Practice

---
+++ 03_tailwind_css/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 p-4 text-black rounded-xl mb-4'>TailwindCSS</h1>
+
+      <Card username="Hirdesh"/>
+
+    </>
+  )
+}
+
+export default App
