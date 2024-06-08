ec5694104be5af87c0e789d89a5bcc4d48601a2d
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-06-08 16:25:01 +0530

    Context API Practice

---
+++ 08_miniContext/src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+import { useContext } from 'react'
+import userContext from '../context/UserContext';
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { setUser } = useContext(userContext);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setUser({ username, password })
+    }
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type='text' placeholder='username' value={username} onChange={e => setUsername(e.target.value)} />
+            <input type='text' placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
+            <button onClick={handleFormSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
